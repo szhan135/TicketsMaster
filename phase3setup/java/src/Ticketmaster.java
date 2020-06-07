bf637e98f878ebb13327e6eab869cbb8d9334e95
jxu103 <43682343+jxu103@users.noreply.github.com>
2020-06-07 16:29:27 -0700

    Update Ticketmaster.java

--- phase3setup/java/src/Ticketmaster.java
+++ phase3setup/java/src/Ticketmaster.java
@@ -349,7 +349,7 @@ public class Ticketmaster{
 			String email = in.readLine();
 
 			int row = (esql.executeQueryAndReturnResult("Select email from Users where email = \"" + email + "\"")).size();
-			System.out.print("Select email from Users where email = \"" + email + "\"")
+			System.out.print("Select email from Users where email = \"" + email + "\"");
 			while(row == 0) {
 				System.out.print("Invalid email, Enter User Email: ");
 				email = in.readLine();
