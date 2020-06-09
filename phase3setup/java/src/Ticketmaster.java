95aad2eae240d43a846d2247d715b4aa55081167
jxu103 <43682343+jxu103@users.noreply.github.com>
2020-06-08 18:00:25 -0700

    Update Ticketmaster.java

--- phase3setup/java/src/Ticketmaster.java
+++ phase3setup/java/src/Ticketmaster.java
@@ -24,6 +24,7 @@ import java.io.InputStreamReader;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Date;
+import java.text.SimpleDateFormat;
 
 /**
  * This class defines a simple embedded SQL utility class that is designed to
